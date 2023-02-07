feb7e1e1a26e3348e3f053cfdfe97dd0c84cd846
Mialy05 <mialisoamurielle@gmail.com>
2023-02-07 21:49:41 +0300

    [/models/Objet_model] fonction getAll et getCondition; [/controllers/Userhome.php] controller qui charge la vue pour les objets des autres propriétaires

---
+++ application/models/Objet_model.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ *
+ * Model Objet_model
+ *
+ * This Model for ...
+ * 
+ * @package		CodeIgniter
+ * @category	Model
+ * @author    Setiawan Jodi <jodisetiawan@fisip-untirta.ac.id>
+ * @link      https://github.com/setdjod/myci-extension/
+ * @param     ...
+ * @return    ...
+ *
+ */
+
+class Objet_model extends CI_Model {
+
+  // ------------------------------------------------------------------------
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  // ------------------------------------------------------------------------
+
+
+  // ------------------------------------------------------------------------
+  public function index()
+  {
+    // 
+  }
+
+  public function getAll() {
+    $query = $this->db->get("objet");
+    return $query->result();
+  }
+
+  public function getCondition($condition) {
+    $query = $this->db->get_where("objet", $condition);
+    return $query->result();
+  }
+
+  // ------------------------------------------------------------------------
+
+}
+
+/* End of file Objet_model.php */
+/* Location: ./application/models/Objet_model.php */
